27bb230f3c75e4c50522848ed9427c750757748b
Valentina De Mite <valesakura97@gmail.com>
2023-09-28 21:35:35 +0200

    milestone 2 completed minus tv series
    milestone 3 added images

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <script>
 import { state } from "./state.js";
 import axios from "axios";
+import movieCard from "./components/movieCard.vue";
+
 export default {
   name: "App",
   data() {
     return {
       state,
+      api_url:
+        "https://api.themoviedb.org/3/search/movie?api_key=3251d53895cc1c147c63e0202c2fe26d&query=",
     };
+  },
+
+  methods: {
+    callApi(urlMovie) {
+      axios
+        .get(this.api_url + urlMovie)
+        .then((resp) => {
+          this.state.movies = resp.data.results;
+          console.log(resp.data.results);
+        })
+        .catch((err) => {
+          console.error(err.message);
+          this.state.errorMessage = err.message;
+        });
+    },
+
+    Search() {
+      let urlMovie = this.state.searchInput;
+
+      if (urlMovie != "") {
+        console.log(urlMovie);
+
+        this.callApi(urlMovie);
+      } else {
+        console.log("OPS: c'e' statro un problema!");
+      }
+    },
+  },
+
+  components: {
+    movieCard,
   },
 };
 </script>
 
 <template>
-  <div class="container">
-    <div class="searchbox py-5">
-      <input class="w-50 ps-2" type="search" v-model="state.searchInput" />
-      <button>Cerca</button>
+  <body>
+    <div class="container">
+      <div class="searchbox py-5">
+        <input class="w-50 ps-2" type="search" v-model="state.searchInput" />
+        <button @click="Search">Cerca</button>
+      </div>
+
+      <div class="row row-cols-2 row-cols-md-4 g-4 my-5">
+        <movieCard v-for="movie in state.movies" :movie="movie"></movieCard>
+      </div>
     </div>
-  </div>
+  </body>
 </template>
 
-<style></style>
+<style>
+body {
+  background-color: rgb(76, 76, 76);
+}
+</style>
---
+++ src/components/movieCard.vue
@@ -0,0 +1,74 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "movieCard",
+  props: {
+    movie: Object,
+  },
+
+  data() {
+    return {
+      img_url: "http://image.tmdb.org/t/p/w500/",
+    };
+  },
+
+  methods: {
+    getFlag_img(original_language) {
+      if (original_language === "en") {
+        return "src/assets/img/united_kingdom_flags_flag_17079.ico";
+      }
+      if (original_language === "it") {
+        return "src/assets/img/italy_flags_flag_17018.ico";
+      }
+      if (original_language === "ja") {
+        return "src/assets/img/japan_flags_flag_17019.ico";
+      }
+      if (original_language === "es") {
+        return "src/assets/img/spain_flags_flag_17068.ico";
+      }
+      if (original_language === "ru") {
+        return "src/assets/img/russia_flags_flag_17058.ico";
+      }
+      if (original_language === "fr") {
+        return "src/assets/img/france_flags_flag_16999.ico";
+      }
+      if (original_language === "ge") {
+        return "src/assets/img/germany_flags_flag_17001.ico";
+      } else {
+        return "src/assets/img/1497554326-multiple-flags_84867.png";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="col">
+    <div class="card rounded">
+      <img class="img-fluid poster" :src="img_url + movie.poster_path" alt="" />
+      <div class="card-text text-start pt-5 ps-2">
+        <h5>Titolo: {{ movie.title }}</h5>
+        <h5>Titolo originale: {{ movie.original_title }}</h5>
+        <p>Media di voti: {{ movie.vote_average }}</p>
+        <p>
+          Lingua Originale:
+          <img class="flag pb-3" :src="getFlag_img(movie.original_language)" />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card {
+  height: 100%;
+}
+
+.poster {
+  height: 50%;
+}
+.flag {
+  width: 15%;
+}
+</style>
