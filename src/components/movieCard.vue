4a29339b33622b6c7c43c89ed9abe52a090d307d
Valentina De Mite <valesakura97@gmail.com>
2023-10-01 20:51:36 +0200

    completed milestone 3

--- src/components/movieCard.vue
+++ src/components/movieCard.vue
@@ -10,6 +10,7 @@ export default {
   data() {
     return {
       img_url: "http://image.tmdb.org/t/p/w500/",
+      starHTML: "",
     };
   },
 
@@ -45,6 +46,28 @@ export default {
       }
     },
   },
+  created() {
+    this.results.vote = Math.round(this.results.vote / 2);
+    console.log(this.results.vote);
+
+    let starsMarkup = "";
+
+    for (let i = 0; i < this.results.vote; i++) {
+      starsMarkup += `<svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-star-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+          />
+        </svg>`;
+    }
+    this.starHTML = starsMarkup;
+  },
 };
 </script>
 
@@ -56,6 +79,8 @@ export default {
         <h5>Titolo: {{ results.title }}</h5>
         <h5>Titolo originale: {{ results.o_title }}</h5>
         <p>Media di voti: {{ results.vote }}</p>
+        <div v-html="starHTML"></div>
+
         <p>
           Lingua Originale:
           <img class="flag pb-3" :src="getFlag_img(results.language)" />
@@ -75,5 +100,9 @@ export default {
 }
 .flag {
   width: 15%;
+}
+
+svg {
+  fill: #fff71a;
 }
 </style>
